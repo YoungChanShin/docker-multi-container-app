8244ed98e6215fab3907384704fdb018e02f598a
Youngchan Shin <chan8149@naver.com>
2020-11-21 15:04:38 +0900

    Add version in docker images

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql")
+const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
@@ -6,6 +6,6 @@ const pool = mysql.createPool({
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_PORT
-})
+});
 exports.pool = pool
 
